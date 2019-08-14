df75501969ead5310605e0c5a260d69bdea5a76d
NickHolke <nickholke@gmail.com>
2019-08-13 18:10:22 -0700

    removing bubbles from array

--- sketch.js
+++ sketch.js
@@ -13,8 +13,10 @@ function draw() {
 
 function mousePressed() {
   for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].clicked();
+    if (bubbles[i].contains()) {
+      bubbles.splice(i, 1);
    }
+  }
 }
 
 function keyTyped() {
@@ -38,25 +40,20 @@ class Bubble {
    if (this.display) {
     strokeWeight(2);
     stroke(255);
-    let d = dist(mouseX, mouseY, this.x, this.y)
-    if (d <= this.r) {
+    if (this.contains()) {
       fill(255, 255, 255, 220);
     } else {
-     noFill();
+      noFill();
     }
-    
     ellipse(this.x, this.y, this.r, this.r);
    }
  }
 
- clicked() {
+ contains() {
   let d = dist(mouseX, mouseY, this.x, this.y)
-  if (d <= this.r) {
-    this.display = false;
-  }
+  return d <= this.r;
  }
 
-  
  move() {
    this.x = this.x + random(-1,1);
    this.y = this.y + random(-1,1);
